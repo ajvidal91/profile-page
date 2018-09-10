8fcbb6023c20c24f1bc77fb5b94a6080c8009d00
ajvidal91 <angelvidaldev@gmail.com>
2018-09-10 12:05:51 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $("#heart").on("click", function(){
+        $("#heart").toggleClass("fas")
+            $("#heart").css("color", "#dd002c");
+    })
+
+    $("#up").on("click", function(){
+        $("#up").toggleClass("fas")
+            $("#up").css("color", "#1c00bc")
+    })
+
+    $("#down").on("click", function(){
+        $("#down").toggleClass("fas")
+            $("#down").css("color", "#5b147a")
+    })
+})
